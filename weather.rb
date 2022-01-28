715cf790d4ac31ab2b11c000689fae82bd52e1b3
Ariel Schwalb <ajschwalb@gmail.com>
2022-01-28 16:04:59 +0200

    weather

---
+++ weather.rb
@@ -0,0 +1,30 @@
+
+# Declare variable.
+
+weather = 'sunny'
+
+# Write the case statement. Decide what to do in each situation.
+
+case weather
+
+when 'sunny' # When it is sunny...
+
+	puts "Grab those sunglasses because you're gonna need em!" # wear sunglasses.
+
+when 'snowy' # When it is snowy...
+
+	puts "Strap on those snowboots!" # wear snowboots.
+
+when 'rainy' # When it is rainy...
+
+	puts "Don't forget to pack your umbrella!" # pack an unmbrella. 
+
+when 'hailing' # When it is hailing...
+
+	puts "Stay home. It's dangerous out there."  # stay home.
+
+else
+
+	puts "Hmm, well I don't know what kind of weather that is!"
+    
+end
